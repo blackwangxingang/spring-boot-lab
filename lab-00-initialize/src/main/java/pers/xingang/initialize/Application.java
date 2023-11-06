45dd1f775681aa4aa6679469fd72aaea3145bf10
wangxingang <wangxgmail@163.com>
2023-11-06 16:25:07 +0800

    初始化项目

---
+++ lab-00-initialize/src/main/java/pers/xingang/initialize/Application.java
@@ -0,0 +1,16 @@
+package pers.xingang.initialize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 应用启动类
+ * @author 王新刚
+ * @date 2023/11/06 16:04
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
