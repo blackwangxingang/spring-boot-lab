842a0ef3a1dcbbcd9e0c3505bd5003e0e2ca214b
wangxingang <wangxgmail@163.com>
2023-11-09 00:06:32 +0800

    lab-04 打印SpringBoot的Bean

---
+++ lab-04/src/main/java/pers/xingang/demo/Application.java
@@ -0,0 +1,33 @@
+package pers.xingang.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+/**
+ * @author 王新刚
+ * @since 2023/11/08
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("让我们查询一下SpringBoot提供的Beans：");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
+}
